caf2f80869f00beb44e04eed7577fb802f55a101
Erik van der Maden <Erik@conneqtech.com>
2018-03-02 14:58:54 +0100

    added a basic backstory generator

--- prerun.index.php
+++ prerun.index.php
@@ -29,6 +29,54 @@ $races = [
     'Tiefling'
 ];
 
+//todo add more traits
+//you're $trait who $verbsaboutraces $race $reasonaboutraces
+//you're $trait who &personalreason
+$traits = [
+    'an adventurous', 'an agreeable', 'an anxious', 'an athletic', 'an apathetic',
+    'a brilliant', 'a benevolent', 'a brave', 'a bitter', 'a bossy',
+    'a clever', 'a calm', 'a clumsy', 'a childish', 'a confident',
+    'a daring', 'a determined', 'a dementing', 'a deadly', 'a dull',
+    'an enthusiastic', 'an empathetic', 'an eccentric', 'an educated',
+    'a friendly', 'a foolish', 'a forgetful', 'aforgiving', 'a freethinking',
+    'a gay', 'a generous', 'a greedy', 'a graceful',
+    'a helpful', 'an honorable', 'a heroic', 'a humble',
+    'an independent', 'an insightful', 'intuitive',
+    'a jealous', 'a jolly', 'a jack of all trades',
+    'a kind'
+];
+
+//___ who ___
+$verbsAboutRaces = [
+    'hates', 'loves', 'fears', 'avoids', 'likes', 'despises', 'adores', 'fights'
+];
+
+//___ because ___
+$reasonsAboutRaces = [
+    'of a childhood trauma',
+    'his father never hugged him',
+    'of the way their behaviour influences the world',
+    'they remind him of his aunt',
+    'he is jealous of their aesthetic',
+    'they make the world go round',
+    'of their work ethic',
+    'of personal reasons',
+    'they ruin everything',
+    'they cook the best types of food',
+    'his childhood friend was one'
+];
+
+$personalReason =[
+    'is recovering from a freak accident at the mine',
+    "hasn't worked a day in his life because of a clever scheme that recently fell apart",
+    'is obsessed with a game where he pretends to be an IT-er in a world with computers and no magic',
+    'tries to cause chaos in any form of organized government',
+    'only wanted to be left alone but keeps getting drug into things',
+    "hasn't taken a bath since the age of 14",
+    'is trying his darnedest never to be sober'
+];
+
+
 //make a pc object for data storage
 $pc = new pc();
 
@@ -43,6 +91,15 @@ $pc->setCha(rollstat());
 //set random race and class
 $pc->setRace($races[array_rand($races)]);
 $pc->setClass($classes[array_rand($classes)]);
+
+$pc->setTrait($traits[array_rand($traits)]);
+
+$backstory = [
+    "you're a " . $pc->getRace() . ' ' . $pc->getClass() . ' who ' . $verbsAboutRaces[array_rand($verbsAboutRaces)] . ' ' . $races[array_rand($races)] . 's because ' . $reasonsAboutRaces[array_rand($reasonsAboutRaces)],
+    "you're a " . $pc->getRace(). ' ' . $pc->getClass() . ' who ' . $personalReason[array_rand($personalReason)]
+];
+
+$pc->setBackstory($backstory[array_rand($backstory)]);
 
 function rollstat() {
     //make 4 d6's
@@ -73,6 +130,8 @@ function rollstat() {
 
     //subtract the lowest value
     $total -= min($diceValues);
-
     return $total;
 }
+
+
+
