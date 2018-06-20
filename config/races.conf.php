b47be6164a4a36f266d15ffffc318925df9bb835
Tim van Riet <tvanriet@syntess.nl>
2018-06-20 14:33:36 +0200

    Added a (incomplete) race overview

---
+++ config/races.conf.php
@@ -0,0 +1,103 @@
+<?php
+$races = [
+    'Dwarf' => [
+        'stats' => [
+            'str' => 2,
+            'con' => 2
+        ],
+        'features' => [
+            'Resistance to poison damage',
+            'Darkvision (120 feet)',
+            'Proficiency on checks related to stonework'
+        ]
+    ],
+    'High Elf' => [
+        'stats' => [
+            'int' => 2,
+            'dex' => 1
+        ],
+        'features' => [
+            'Daksvision (60 feet)',
+            'Get a free wizard cantrip',
+            'Long rests only take four hours of meditation'
+        ]
+    ],
+    'Wood Elf' => [
+        'stats' => [
+            'wis' => 1,
+            'dex' => 2
+        ],
+        'features' => [
+            'Long rests only take four hours of meditation',
+            'You are even able to hide in rain'
+        ]
+    ],
+    'Halfling' => [
+        'stats' => [
+            'con' => 2,
+            'cha' => 1
+        ],
+        'features' => [
+            'lucky'
+        ]
+    ],
+    'Human' => [
+        'stats' => [
+            'str' => 1,
+            'dex' => 1,
+            'con' => 1,
+            'int' => 1,
+            'wis' => 1,
+            'cha' => 1
+        ],
+        'features' => [
+            'nothing special'
+        ]
+    ],
+    'Dragonborn' => [
+        'stats' => [
+            'con' => 1,
+            'cha' => 2
+        ],
+        'features' => [
+            'breath weapon'
+        ]
+    ],
+    'Gnome' => [
+        'stats' => [
+            'dex' => 2,
+            'int' => 2
+        ],
+        'features' => [
+            'darkvision (60 feet)'
+        ]
+    ],
+    'Half-Elf' => [
+        'stats' => [
+            'int' => 1,
+            'cha' => 2
+        ],
+        'features' => [
+            'Long rests only take four hours of meditation',
+            'darkvision (60 feet)'
+        ]
+    ],
+    'Half-Orc' => [
+        'stats' => [
+            'str' => 2,
+            'con' => 1
+        ],
+        'features' => [
+            'TODO: Ik heb zo uit mijn hoofd werkelijk geen idee'
+        ]
+    ],
+    'Tiefling' => [
+        'stats' => [
+            'cha' => 2,
+            'wis' => 1
+        ],
+        'features' => [
+            'darkvision (60 feet)'
+        ]
+    ]
+];
