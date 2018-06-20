02a3591ed48b9004b970e30340e1c54b227cb6e7
Tim van Riet <tvanriet@syntess.nl>
2018-06-20 14:45:03 +0200

    Moved the traits to their own config

--- prerun.index.php
+++ prerun.index.php
@@ -2,66 +2,8 @@
 require_once 'models/dice.class.php';
 require_once 'models/pc.class.php';
 require_once 'config/classes.conf.php';
-
-$races = [
-    'Dwarf',
-    'Elf',
-    'Halfling',
-    'Human',
-    'Dragonborn',
-    'Gnome',
-    'Half-Elf',
-    'Half-Orc',
-    'Tiefling'
-];
-
-//todo add more traits
-//you're $trait who $verbsaboutraces $race $reasonaboutraces
-//you're $trait who &personalreason
-$traits = [
-    'an adventurous', 'an agreeable', 'an anxious', 'an athletic', 'an apathetic',
-    'a brilliant', 'a benevolent', 'a brave', 'a bitter', 'a bossy',
-    'a clever', 'a calm', 'a clumsy', 'a childish', 'a confident',
-    'a daring', 'a determined', 'a dementing', 'a deadly', 'a dull',
-    'an enthusiastic', 'an empathetic', 'an eccentric', 'an educated',
-    'a friendly', 'a foolish', 'a forgetful', 'aforgiving', 'a freethinking',
-    'a gay', 'a generous', 'a greedy', 'a graceful',
-    'a helpful', 'an honorable', 'a heroic', 'a humble',
-    'an independent', 'an insightful', 'intuitive',
-    'a jealous', 'a jolly', 'a jack of all trades',
-    'a kind'
-];
-
-//___ who ___
-$verbsAboutRaces = [
-    'hates', 'loves', 'fears', 'avoids', 'likes', 'despises', 'adores', 'fights'
-];
-
-//___ because ___
-$reasonsAboutRaces = [
-    'of a childhood trauma',
-    'his father never hugged him',
-    'of the way their behaviour influences the world',
-    'they remind him of his aunt',
-    'he is jealous of their aesthetic',
-    'they make the world go round',
-    'of their work ethic',
-    'of personal reasons',
-    'they ruin everything',
-    'they cook the best types of food',
-    'his childhood friend was one'
-];
-
-$personalReason =[
-    'is recovering from a freak accident at the mine',
-    "hasn't worked a day in his life because of a clever scheme that recently fell apart",
-    'is obsessed with a game where he pretends to be an IT-er in a world with computers and no magic',
-    'tries to cause chaos in any form of organized government',
-    'only wanted to be left alone but keeps getting drug into things',
-    "hasn't taken a bath since the age of 14",
-    'is trying his darnedest never to be sober'
-];
-
+require_once 'config/races.conf.php';
+require_once 'config/traits.conf.php';
 
 //make a pc object for data storage
 $pc = new pc();
@@ -74,7 +16,7 @@ for($i = 0; $i < 6; $i++) {
 $stats = sortStats($stats);
 
 //set random race and class
-$pc->setRace($races[array_rand($races)]);
+$pc->setRace($raceNames[array_rand($raceNames)]);
 $pc->setClass(array_rand($classes));
 
 //set the stats required for the class
@@ -107,7 +49,7 @@ foreach($classes[$pc->getClass()]['statOrder'] as $i => $stat) {
 $pc->setTrait($traits[array_rand($traits)]);
 
 $backstory = [
-    "you're a " . $pc->getRace() . ' ' . $pc->getClass() . ' who ' . $verbsAboutRaces[array_rand($verbsAboutRaces)] . ' ' . $races[array_rand($races)] . 's because ' . $reasonsAboutRaces[array_rand($reasonsAboutRaces)],
+    "you're a " . $pc->getRace() . ' ' . $pc->getClass() . ' who ' . $verbsAboutRaces[array_rand($verbsAboutRaces)] . ' ' . $raceNames[array_rand($raceNames)] . 's because ' . $reasonsAboutRaces[array_rand($reasonsAboutRaces)],
     "you're a " . $pc->getRace(). ' ' . $pc->getClass() . ' who ' . $personalReason[array_rand($personalReason)]
 ];
 
